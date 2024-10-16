deafcc55afaad70a787ca72fc530bffc36bb579c
aschmitt 42 <alban.schmitt@hotmail.com>
2024-10-16 19:26:28 +0200

    C05

--- cpp05/ex01/Bureaucrat.cpp
+++ cpp05/ex01/Bureaucrat.cpp
@@ -1,2 +1,14 @@
 #include "Bureaucrat.hpp"
 
+Bureaucrat::Bureaucrat(void)
+{
+    _Name = "";
+    _Grade = 150;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade)
+{
+    std::cout << "Bureaucrat default constructor called." << std::endl;
+    _Name = name;
+    _Grade = grade;
+}
--- cpp05/ex01/Bureaucrat.hpp
+++ cpp05/ex01/Bureaucrat.hpp
@@ -10,6 +10,19 @@ class Bureaucrat
     private :
         const std::string _Name;
         int _Grade
+    public :
+        Bureaucrat(void);
+        Bureaucrat(std::string name, int grade);
+
+
+        class GradeTooHighException : public std::exception {
+			public:
+				virtual const char *	what() const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char *	what() const throw();
+		};
 };
 
 #endif
