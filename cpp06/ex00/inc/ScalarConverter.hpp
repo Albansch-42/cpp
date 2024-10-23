3cd85ad677cb7ec52890cfe7a7a473ea730abc8c
aschmitt 42 <alban.schmitt@hotmail.com>
2024-10-23 18:53:38 +0200

    debut c06

--- cpp06/ex00/inc/ScalarConverter.hpp
+++ cpp06/ex00/inc/ScalarConverter.hpp
@@ -1,17 +1,27 @@
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
-# include <iostream>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <climits>
+#include <limits.h>
+#include <float.h>
+#include <cstdlib>
+
 
 class ScalarConverter
 { 
     private :
-
-    public :
         ScalarConverter();
-        ~ScalarConverter();
         ScalarConverter(const ScalarConverter &other);
         ScalarConverter& operator=(const ScalarConverter &other);
+        ~ScalarConverter();
+    
+    public :
+        static void convert(std::string s);
+
+
 };
 
 // std::ostream& operator<<(std::ostream& os, const ScalarConverter& other);
--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -1,18 +1,48 @@
-#include "inc/ScalarConverter.hpp"
+#include "ScalarConverter.hpp"
 
 ScalarConverter::ScalarConverter()
 {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 }
 ScalarConverter::~ScalarConverter()
 {
-	std::cout << "Destructor called" << std::endl;
+	 // std::cout << "Destructor called" << std::endl;
 }
 ScalarConverter::ScalarConverter(const ScalarConverter&other)
 {
-	std::cout << "Recopy constructor called" << std::endl;
+	(void)other;
+	 // std::cout << "Recopy constructor called" << std::endl;
 }
 ScalarConverter& ScalarConverter::operator=(const ScalarConverter &other)
 {
-	std::cout << "Assignment operator called" << std::endl;
+	(void)other;
+	return (*this);
+	// std::cout << "Assignment operator called" << std::endl;
+}
+
+int	is_int(std::string s)
+{
+	for (size_t i = 0; i < s.length(); i++)
+	{
+		if (!isdigit(s[i]))
+			return (1);
+	}
+	return 1;
+}
+
+void ScalarConverter::convert(std::string s)
+{
+	if (is_int(s)){
+		int n = atof(s.c_str());
+		if (n < 0 || n > 127)
+			std::cout << "char: impossible" << std::endl;
+		else if (n < 32 || n == 127)
+			std::cout << "char: non-displayable" << std::endl;
+		else
+			std::cout << "char: " << static_cast<char>(n) << std::endl;
+		std::cout << "int: " << n << std::endl;
+		std::cout << "float: " << std::fixed << std::setprecision(2) << n << "f" << std::endl;
+		std::cout << "double: " << std::fixed << std::setprecision(2) << n << std::endl;
+	}
+	
 }
